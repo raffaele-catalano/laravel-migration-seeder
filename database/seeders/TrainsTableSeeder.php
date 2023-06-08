602e3c3b9a2d61410174813f88766b1e4b5e4009
raffaele-catalano <raf.catalano@gmail.com>
2023-06-08 18:35:24 +0200

    added 10 more fake trains + changed some fakers

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -19,17 +19,19 @@ class TrainsTableSeeder extends Seeder
     public function run(Faker $faker)
     {
 
-        for ($i=0; $i < 10; $i++) {
+        for ($i=0; $i < 20; $i++) {
             # code...
 
             $train = new Train();
-            $train->company = $faker->word();
+            $train->company = $faker->randomElement(['Trenitalia', 'Italo']);
             $train->departure_station = $faker->city();
             $train->arrival_station = $faker->city();
             $train->departure_time = $faker->date() . ' ' . $faker->time();
             $train->arrival_time = $faker->date() . ' ' . $faker->time();
             $train->train_code = $faker->isbn10();
-            $train->wagon_number = $faker->randomDigit();
+            $train->wagon_number = $faker->numberBetween(1, 15);
+            $train->is_in_time = rand(0, 1);
+            $train->is_cancelled = rand(0, 1);
 
             $train->save();
         }
