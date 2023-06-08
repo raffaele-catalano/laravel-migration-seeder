76f19f53030ebe039b399679a05745291b72bf67
raffaele-catalano <raf.catalano@gmail.com>
2023-06-08 16:22:04 +0200

    created table 'trains' - migration

---
+++ database/migrations/2023_06_08_140516_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 80);
+            $table->string('arrival_station', 80);
+            $table->dateTime('departure_time');
+            $table->dateTime('arrival_time');
+            $table->string('train_code', 10)->unique();
+            $table->tinyInteger('wagon_number');
+            $table->boolean('is_in_time');
+            $table->boolean('is_cancelled');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
